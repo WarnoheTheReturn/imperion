69a643cd3b48fdef74daf6bcd981367ab9ff1d9e
= <warnohexd@email.com>
2026-05-26 18:24:34 +0200

    administrative page

---
+++ frontend/src/pages/PP.tsx
@@ -0,0 +1,11 @@
+function PP() {
+    return (
+        <div>
+            <h1>Privacy Policy</h1>
+            <p>We take your privacy seriously. We do not collect any personal information from our users. We do not share any information with third parties. We do not use cookies or any other tracking technologies.</p>
+            <p>If you have any questions about our privacy policy, please contact us at <a href="mailto:6yDyK@example.com">6yDyK@example.com</a></p>
+        </div>
+    );
+}
+
+export default PP;
---
+++ frontend/src/pages/TOS.tsx
@@ -0,0 +1,15 @@
+function Tos() {
+  return (
+    <div className="tos-container">
+      <h1>Terms of Service</h1>
+      <p>Welcome to the imperion bot! By using our services, you agree to the following terms and conditions:</p>
+      <ul>
+        <li>Use the bot at your own risk.</li>
+        <li>Do not use the bot for illegal purposes.</li>
+        <li>Do not use the bot to harass or bully others.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Tos;
