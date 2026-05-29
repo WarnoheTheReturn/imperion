f91aa75e094da873c64d6b3d268af66789b20b71
Warnohe <warnohexd@email.com>
2026-05-29 17:28:06 +0200

    update footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,17 +2,30 @@ import styles from './Footer.module.css'
 
 function Footer() {
     return (
+        <>
+        <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
         <footer className={styles.footer}>
             <div className={styles.footerContainer}>
-                <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-                <p>&copy; {new Date().getFullYear()} Impérion. All rights reserved.</p>
-                <div className={styles.footerLinks}>
-                    <a href="/tos">Terms of Service · </a>
-                    <a href="/pp">Privacy Policy · </a>
-                    <a href="#contact">Contact Us</a>
+                <div className={styles.footerHeader}>
+                    <div>
+                        <p>Impérion</p>
+                    </div>
+                    <div >
+                        <p>Légal</p>
+                        <div className={styles.footerLinks}>
+                            <a href="/tos">Terms of Service</a>
+                            <a href="/pp">Privacy Policy</a>
+                            <a href="#contact">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.footerButtom}>
+                    <p>&copy; {new Date().getFullYear()} Impérion. All rights reserved.</p>
+                    <p>Made with ❤️ by Warnohe</p>
                 </div>
             </div>
         </footer>
+        </>
     );
 }
 
