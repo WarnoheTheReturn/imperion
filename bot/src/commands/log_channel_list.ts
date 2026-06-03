76a4336fd2894a64b524e2f96bde1a4355dd3c66
WarnoheTheReturn <warnohexd@gmail.com>
2026-06-03 13:14:23 +0200

    add some commands

---
+++ bot/src/commands/log_channel_list.ts
@@ -0,0 +1,27 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction, TextChannel,EmbedBuilder } from "discord.js";
+import { Command, LogChannelType } from "../types";
+import { Bot } from "../types";
+import { LogsLogChannelRow } from "../db/models/logs_log_channel";
+
+const command: Command = {
+  data: new SlashCommandBuilder()
+    .setName("log_channel_list")
+    .setDescription("list all log channels") as SlashCommandBuilder,
+
+  execute: async (interaction: ChatInputCommandInteraction, bot: Bot) => {
+    const sent = await interaction.deferReply();
+
+    const logChannelsDatas = await bot.db.tables.logs_log_channel.getAll();
+    const embed = new EmbedBuilder()
+      .setTitle("Log Channels")
+      .setColor("#1840da");
+        for (const logChannelData of logChannelsDatas) {
+        const channel = interaction.guild?.channels.cache.get(logChannelData.data.channel_id) as TextChannel;
+        embed.addFields({ name: logChannelData.data.type, value: `<#${channel.id}>` });
+        }
+    await interaction.editReply({ embeds: [embed] });
+
+  },
+};
+
+export default command;
