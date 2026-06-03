197ceaab05e5d1518606e25f67875d34c3973d52
WarnoheTheReturn <warnohexd@gmail.com>
2026-06-03 13:14:19 +0200

    main components

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+function Header() {
+    
+    return (
+        <header className="header">
+            <h1>Impérion</h1>
+            <h2>Une bot pour faire entrer l'empire francais dans une nouvelle dimension</h2>
+        </header>
+    );
+}
+
+export default Header;
