972dbf5d1fb6db7f78051c7bca33499c9fb970f1
WarnoheTheReturn <warnohexd@gmail.com>
2026-06-03 13:14:21 +0200

    server link

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,30 @@ import { client } from "./client";
 import { loadCommands } from "./handlers/commandHandler";
 import { loadEvents } from "./handlers/eventHandler";
 import { config } from './config';
+import { createServer } from "./api/server";
 
 
-(async () => {
-  await loadCommands(client);
-  await loadEvents(client);
-  await client.login(config.discord.token);
-})();
+
+
+async function bootstrap() {
+  try {
+      console.log("Démarrage...");
+
+      await loadCommands(client);
+      await loadEvents(client);
+      
+      const app = createServer(client);
+
+      await client.login(config.discord.token);
+      
+      app.listen(config.api.port, () => {
+          console.log(`✅ Serveur web en ligne sur le port ${config.api.port}`);
+      });
+
+  } catch (error) {
+      console.error("❌ Erreur critique lors du démarrage :", error);
+      process.exit(1); 
+  }
+}
+
+bootstrap();
