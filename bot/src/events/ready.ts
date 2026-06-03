90e30860aabe8b5abbb62f0c9c7119d48370f6ea
WarnoheTheReturn <warnohexd@gmail.com>
2026-06-03 13:14:23 +0200

    refactor

--- bot/src/events/ready.ts
+++ bot/src/events/ready.ts
@@ -1,10 +1,9 @@
-import { Client } from "discord.js";
-import { Event } from "../types";
+import { Event, Bot } from "../types";
 
 const event: Event = {
   name: "clientReady",
   once: true,
-  async execute(client: Client) {
+  async execute(client: Bot) {
     console.log(`✅ Bot connected as ${client.user?.tag}`);
   },
 };
