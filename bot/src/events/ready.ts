f2f47408ab26f65fcebc9943cf5df8ed04818efd
WarnoheTheReturn <warnohexd@gmail.com>
2026-06-03 13:14:23 +0200

    preload discord member

--- bot/src/events/ready.ts
+++ bot/src/events/ready.ts
@@ -4,6 +4,7 @@ const event: Event = {
   name: "clientReady",
   once: true,
   async execute(client: Bot) {
+    await client.guilds.cache.first()?.members.fetch();
     console.log(`✅ Bot connected as ${client.user?.tag}`);
   },
 };
