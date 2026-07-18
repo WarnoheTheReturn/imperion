4343dc1a3932eac7abd75f0eebf49486342b7767
WarnoheTheReturn <warnohexd@gmail.com>
2026-07-18 22:18:24 +0200

    compilation fix

--- bot/src/generated/version.ts
+++ bot/src/generated/version.ts
@@ -1,4 +1,4 @@
 // DO NOT MODIFY THIS FILE
-export const BUILD_NUMBER = 104;
-export const BUILD_SHA = '7d322bb';
-export const BOT_VERSION = '0.1.104';
+export const BUILD_NUMBER = 105;
+export const BUILD_SHA = 'e53d341';
+export const BOT_VERSION = '0.1.105';
