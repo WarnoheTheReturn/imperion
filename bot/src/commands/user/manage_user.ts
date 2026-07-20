b8f2b0383288ad58f9a7358cf991f403684595f7
WarnoheTheReturn <warnohexd@gmail.com>
2026-07-20 18:44:44 +0200

    enable to change rank

--- bot/src/commands/user/manage_user.ts
+++ bot/src/commands/user/manage_user.ts
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction,User, EmbedBuilder, MessageFlags , PermissionFlagsBits, InteractionContextType } from "discord.js";
+import { SlashCommandBuilder, ChatInputCommandInteraction,User, EmbedBuilder, MessageFlags , PermissionFlagsBits, InteractionContextType, Role } from "discord.js";
 import { Command } from "../../types";
 import { Bot } from "../../types";
 import { UsersModel } from "../../db/models/users"
@@ -17,6 +17,10 @@ const command: Command = {
     .addNumberOption((option) => option
         .setName("roblox_id")
         .setDescription("The roblox id")
+    )
+    .addRoleOption((option) => option
+        .setName("rank")
+        .setDescription("The rank")
     ) as SlashCommandBuilder,
 
   execute: async (interaction: ChatInputCommandInteraction, bot: Bot) => {
@@ -24,14 +28,33 @@ const command: Command = {
 
     const user = interaction.options.getUser("user") as User;
     const roblox_id = interaction.options.getNumber("roblox_id") as number;
+    const rank = interaction.options.getRole("rank") as Role;
+
+    if (!user && !rank) {
+      await interaction.editReply({ content : `❌ Please provide either a user or a rank!` });
+    }
 
     const userData : UsersModel | null = await bot.db.tables.users.getById(user.id);
+    
     if (!userData) {
       await interaction.editReply({ content : `❌ ${user.globalName} is not enlisted !` });
       return;
     }
 
-    userData.data.roblox_id = roblox_id;
+    if (roblox_id) {
+      userData.data.roblox_id = roblox_id;
+    } 
+    if (rank) {
+      const rankData = await bot.db.tables.grades.getById(rank.id);
+      if (!rankData) {
+        await interaction.editReply({ content : `❌ Rank not found !` });
+        return;
+      } 
+      userData.data.current_grade = rankData.data.role_id;
+    }
+
+
+
     await userData.save();
     
     await interaction.editReply({ content : `✅ ${user.globalName} updated !` });
